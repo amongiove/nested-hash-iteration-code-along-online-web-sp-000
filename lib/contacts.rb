7be6f98053748fdf7f431b040379dceffe457850
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-15 14:23:43 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,12 +19,7 @@ def remove_strawberry(contacts)
     if person == "Freddy Mercury"
       data.each do |attribute, value|
         if attribute == :favorite_ice_cream_flavors
-          value.each do |flavor|
-            if flavor == "strawberry"
-              attribute.delete(flavor)
-
-            end
-          end
+          value.delete_if {|flavor| flavor == "strawberry"
         end
       end
     end
